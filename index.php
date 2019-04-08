2bd9d762d3758a0d84a940fec67020147ef19a55
nebroc <nebroc@users.noreply.github.com>
2019-04-07 22:45:09 -0500

    look and feel y estandarizacion
    Se coloco encabezado y footer como instancia en cada pagina

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Logica de programación</title>
+  <link rel="shorcut icon" type="image/x-icon" href="https://www.unad.edu.co/templates/unad2018home/favicon.ico">
+  <?php 
+  $ruta = ""; 
+  ?>
+  <link rel="stylesheet" type="text/css" href="<?php echo $ruta; ?>css/general.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $ruta; ?>css/bootstrap.min.css"/>
+  <link rel="stylesheet" type="text/css" href="<?php echo $ruta; ?>css/menu.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $ruta; ?>css/animate.css"/>
+</head>
+
+<body>
+  <!-- Header -->
+  <?php 
+  @ require_once ($ruta.'contents/encabezado.php'); 
+  ?>
+
+
+  <main>
+    <!-- Contenedor principal de la pagina web -->
+    <section id="Logica" class="container">
+
+
+      <p>
+        OVI diseñado por estudiantes de la carrera ingenieria de sistemas de la universidad Nacional Abieryta y a Distancia - UNAD
+        con la finalidad de dar a conocer el temario de logica de programación donde daremos a conocer temas que se deben abordar para 
+      el aprendizaje y el fortalecimiento del mismo. </br> </br>
+
+    Tambien a traves de este OVI el estudiante podrá: </br>
+
+    <ol>
+      <li>Interpretar de manera adecuada las indicaciones dadas en la guía de actividades del momento 2 del curso.</li>
+      <li>Relacionar las bases teóricas de la metodología del aprendizaje basado en problemas y el contexto del problema dado  en la guía de actividades del momento 2 del curso.</li>
+    </ol>    
+
+    <h2> Objetivos - OVI </h2> </br>
+    <ol>
+      <li>Presenatar de forma clara y didactica informacion sobre el tema de logica de programación.</li>
+      <li>Aplicar los conocimientos adquiridos sobre el diseño de paginas web utilizando HTML y CSS como herramientas de diseño.</li>
+      <li>Afianar y tener mas claros conceptos de diseño de sitios web.</li>
+    </ol>     
+
+    <h2> Objetivos del curso </h2> </br>
+    <ol>
+      <li>Diseñar una interfaz amigable que estimule el proceso de adquisición de nuevo conocimiento en los estudiantes</li>
+      <li>Estructurar un contenido programático acorde con las necesidades de los estudiantes y las competencias requeridas en el contexto actual dentro de un entorno virtual.</li>
+      <li>Aportar un recurso pedagógico que contribuya significativamente al proceso de aprendizaje de los estudiantes en la Ingeniería Web.</li>
+      <li>Contribuir con el proceso investigativo que se adelanta al interior del programa de ingeniería de sistemas de la facultad de ingeniería</li>
+    </ol>                        
+  </p>
+</section>            
+</main>
+
+<!-- Header -->
+  <?php 
+  @ require_once ($ruta.'contents/menuinferior.php'); 
+  ?>
+<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script> 
+<script type="text/javascript" src="js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="js/menu.js"></script>       
+</body>
+</html>
